f8ab651a0fcc9dc4f72cff55624d90b85dcc7d46
Aditya Shetty <adishetty9988@gmail.com>
2023-04-28 11:24:39 +0530

    basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RecipeModel } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipeList: RecipeModel[] = [
+    new RecipeModel('Spagetti', 'Bolognese', 'https://i.pinimg.com/736x/61/51/56/615156cd7381f1b83c9dfbb572efff91.jpg'),
+    new RecipeModel('Baath', 'Bisi Bele Baath', 'https://i.pinimg.com/736x/23/5f/15/235f152f9c8aa926654d7cf8b46b9c21.jpg'),
+    new RecipeModel('Sushi', 'Temakizushi', 'https://i.pinimg.com/736x/94/d1/9f/94d19f6785d47ba3028026996e0eee6b.jpg'),
+    new RecipeModel('Coffee', 'Filter Coffee', 'https://i.pinimg.com/736x/0f/74/56/0f74560312ba6a92f8b75b48e5244d79--best-coffee-hav.jpg')
+  ];
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShoppingListModel } from './shopping-list.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
+  shoppingList: ShoppingListModel[] = [
+    new ShoppingListModel('Tomato', 20),
+    new ShoppingListModel('Rice', 10),
+    new ShoppingListModel('Meat', 15),
+    new ShoppingListModel('Milk', 5)
+  ];
 
 }
